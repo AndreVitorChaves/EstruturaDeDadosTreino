a919ff93771024225f73c8ed85443a48f361904f
Deko <andrevitorchaves.f@gmail.com>
2025-09-26 19:52:08 -0300

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -35,27 +35,126 @@ int minuscula (const char *str){
     return contMinuscula;
 }
 
+int numerico(const char *str){
+
+    int contNumerico = 0;
+    int tamanho = strlen(str);
+
+    for (int i = 0; i < tamanho; i++){
+        if (isdigit(str[i])){
+            contNumerico ++;
+        }
+    }
+
+    return contNumerico;
+}
+
+int especiais(const char *str){
+
+    char caracteres[] = "()*+,-./";
+
+    int contEspeciais = 0;
+    int tamanho = strlen(str);
+
+    for (int i = 0; i < tamanho; i++){
+        if (strchr(caracteres, str[i]) != NULL){
+            contEspeciais ++;
+        }
+    }
+
+    return contEspeciais;
+
+}
+
 int main()
 {
     char senha [50];
 
-    printf("Digite sua senha: ");
+    printf("===============================\n");
+    printf("Bem vindo(a) ao sistema verificador de senhas!\n");
+    printf("===============================\n");
+
+    do{
+    printf("\n Digite sua senha: ");
     scanf("%s", senha);
 
+    
+        if(strlen(senha) < 10){
+        printf("\n Senha invalida! \n Possui menos caracteres do que o minimo esperado (10)! \n");
+            if (maiuscula(senha) < 2){
+                printf("\n Possui menos caracteres maiusculos do que o minimo esperado (2)! \n");
+            }
+            if (minuscula(senha) < 3){
+                    printf("\n Possui menos caracteres minusculos do que o minimo esperado (3)! \n");
+            }
+            if (numerico(senha) < 3){
+                        printf("\n Possui menos caracteres numericos do que o minimo esperado (3)! \n");
+            }
+            if (especiais(senha) < 2){
+                            printf("\n Possui menos caracteres especiais do que o minimo esperado (2)! \n");
+            }          
+        }
 
+        else{
+            if (maiuscula(senha) < 2){
+                printf("\n Senha invalida! \n Possui menos caracteres maiusculos do que o minimo esperado (2)! \n");
+                if (minuscula(senha) < 3){
+                    printf("\n Possui menos caracteres minusculos do que o minimo esperado (3)! \n");
+                }
+                if (numerico(senha) < 3){
+                        printf("\n Possui menos caracteres numericos do que o minimo esperado (3)! \n");
+                }
+                if (especiais(senha) < 2){
+                            printf("\n Possui menos caracteres especiais do que o minimo esperado (2)! \n");
+                }
+            }
 
-    if(strlen(senha) >= 10){
-        printf("Acesso liberado!");
-    }
-    else{
-        printf("Acesso negado!");
-    }
+            else{
+                if (minuscula(senha) < 3){
+                    printf("\n Senha invalida! \n Possui menos caracteres minusculos do que o minimo esperado (3)! \n");
+                    if (numerico(senha) < 3){
+                        printf("\n Possui menos caracteres numericos do que o minimo esperado (3)! \n");
+                    }
+                    if (especiais(senha) < 2){
+                            printf("\n Possui menos caracteres especiais do que o minimo esperado (2)! \n");
+                    }
+                }
+
+                else{
+                    if (numerico(senha) < 3){
+                        printf("\n Senha invalida! \n Possui menos caracteres numericos do que o minimo esperado (3)! \n");
+                        if (especiais(senha) < 2){
+                            printf("\n Possui menos caracteres especiais do que o minimo esperado (2)! \n");
+                        }
+                    }
+
+                    else{
+                        if (especiais(senha) < 2){
+                            printf("\n Senha invalida! \n Possui menos caracteres especiais do que o minimo esperado (2)! \n");
+                        }
+                        else{
+                            printf("Parabens, voce digitou uma senha valida e extremamente forte! \n");
+                        }
+                    }
+                }
+            }
+        }
+    
+
+   
+    }while (strlen(senha) < 10);
 
     int qntMaiusculas = maiuscula(senha);
-    printf ("%i", qntMaiusculas );
+    printf ("\n Letras maiusculas: %i \n", qntMaiusculas );
 
     int qntMinusculas = minuscula(senha);
-    printf ("%i", qntMinusculas );
+    printf ("Letras minusculas: %i \n", qntMinusculas );
 
-   return 0;
-   }
+    int qntNumericos = numerico(senha);
+    printf ("Valores numericos: %i \n", qntNumericos );
+
+    int qntEspeciais = especiais(senha);
+    printf ("Caracteres especiais: %i \n", qntEspeciais );
+
+    return 0;
+}
